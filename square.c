c03e0f86daccd71f028088595e07159fee9dd6e2
Avishkar001 <avirandhale001@gmail.com>
2022-11-11 10:42:23 +0530

    new commit

--- square.c
+++ square.c
@@ -4,6 +4,6 @@ int main(){
 	int num;
 	printf("Enter a number\n");
 	scanf("%d", &num);
-	printf("Square of req number = %d\n", num*num);
+	printf("Square of req num = %d\n", num*num);
 	return 0;
 }
